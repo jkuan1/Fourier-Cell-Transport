ecfb7d50267436ee6b088d8972187deff0602152
Justin <justinkuan2000@gmail.com>
2019-10-15 02:34:59 -0700

    finished q1

--- bmeg371hmwk3q1.m
+++ bmeg371hmwk3q1.m
@@ -4,34 +4,38 @@ k=0.001;
 mesh_step = 1/h;
 
 %BCs
-Grid = zeros([1,mesh_step+1]);
+Grid = zeros([1,mesh_step+1]); %1 by 11 matrix of zeros
 point = 2;
-for x=0+h:h:1-h
+for x= 0+h : h : 1-h %creating initial condition concentration curve 
     Grid(point)=x*(1-x);
     point = point+1;
 end
 
+%starting numerical approximation 
 Grid_forward= Grid;
 Grid_intitial = Grid;
 
 for time=0:k:1
+    
     for point=2:1:length(Grid)-1
-        Grid_forward(point) = Grid(point) + ((k/(h^2))*(Grid(point+1) - 2* Grid(point)+ Grid(point-1)));
+        Grid_forward(point) = Grid(point) + (k/(h^2))*(Grid(point-1) - 2* Grid(point) + Grid(point+1));
     end
     
     Grid = Grid_forward;
+    
+    %saving the concentration profile at these times to graph later
     if time==0.05
-        Grid_point_zero_five = Grid;
+        time_point_zero_five = Grid;
     elseif time==0.1
-        Grid_point_one = Grid;
+        time_point_one = Grid;
     elseif time == 0.2
-        Grid_point_two = Grid;
+        time_point_two = Grid;
     elseif time == 1.00
-        Grid_final = Grid;
+        time_final = Grid;
     end
 end
 
-plot(0:h:1,Grid_intitial, 0:h:1,Grid_point_zero_five, 0:h:1,Grid_point_one, 0:h:1,Grid_point_two, 0:h:1,Grid_final)
+plot(0:h:1,Grid_intitial, 0:h:1,time_point_zero_five, 0:h:1,time_point_one, 0:h:1,time_point_two, 0:h:1,time_final)
 xlabel('0 < x < 1') 
 ylabel('Concentration') 
 title('Concentration vs x')
